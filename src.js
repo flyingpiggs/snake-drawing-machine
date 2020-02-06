8064dd8657b13a0509691ad14685d83bae2f99ff
flyingpiggs <chen172@uic.edu>
2020-02-05 23:26:52 -0600

    added functionality for cheesecake buttons

--- src.js
+++ src.js
@@ -1,4 +1,63 @@
 
-let paragraphs = document.getElementsByTagName( "p" );
+//let paragraphs = document.getElementsByTagName( "p" );
 
-paragraphs[0].innerHTML = "Hello!";   
+//paragraphs[0].innerHTML = "Hello!";  
+
+function DisplayWeird() {
+    let goodElements = document.getElementsByClassName("good");
+    let weirdElements = document.getElementsByClassName("weird");
+    let badElements = document.getElementsByClassName("bad");
+    let weirdButton = document.getElementById( "displayWeird" );
+    let goodButton = document.getElementById( "displayGood" );
+    let badButton = document.getElementById( "displayBad" );
+
+    goodElements[0].style.display = "none";
+    goodElements[1].style.display = "none";
+    goodElements[2].style.display = "none";
+    badElements[0].style.display = "none";
+    weirdElements[0].style.display = "block";
+    weirdElements[1].style.display = "block";
+    weirdButton.style.display = "none"; 
+    badButton.style.display = "inline"; 
+    goodButton.style.display = "inline"; 
+}
+
+function DisplayGood() {
+
+    let goodElements = document.getElementsByClassName("good");
+    let weirdElements = document.getElementsByClassName("weird");
+    let badElements = document.getElementsByClassName("bad");
+    let weirdButton = document.getElementById( "displayWeird" );
+    let goodButton = document.getElementById( "displayGood" );
+    let badButton = document.getElementById( "displayBad" );
+
+    goodElements[0].style.display = "block";
+    goodElements[1].style.display = "block";
+    goodElements[2].style.display = "block";
+    badElements[0].style.display = "none";
+    weirdElements[0].style.display = "none";
+    weirdElements[1].style.display = "none";
+    weirdButton.style.display = "inline"; 
+    badButton.style.display = "inline"; 
+    goodButton.style.display = "none"; 
+}
+
+function DisplayBad() {
+
+    let goodElements = document.getElementsByClassName("good");
+    let weirdElements = document.getElementsByClassName("weird");
+    let badElements = document.getElementsByClassName("bad");
+    let weirdButton = document.getElementById( "displayWeird" );
+    let goodButton = document.getElementById( "displayGood" );
+    let badButton = document.getElementById( "displayBad" );
+
+    goodElements[0].style.display = "none";
+    goodElements[1].style.display = "none";
+    goodElements[2].style.display = "none";
+    badElements[0].style.display = "block";
+    weirdElements[0].style.display = "none";
+    weirdElements[1].style.display = "none";
+    weirdButton.style.display = "inline"; 
+    badButton.style.display = "none"; 
+    goodButton.style.display = "inline"; 
+}
